0f5e07d483e2379fb7801c787a371ecfbc0e513f
surajalhat <myrahulpwar@gmail.com>
2022-02-15 18:26:29 +0530

    Test Oops Concepts

---
+++ oopsConcept/src/TypesofOops/TypesofOops.java
@@ -0,0 +1,20 @@
+package TypesofOops;
+
+class Parent1{
+	void sum() {
+		
+	}
+}
+class Parent2{
+	
+}
+class Parent3 extends Parent2{
+	void sum() {
+		
+	}
+}
+public class TypesOFOopsDemo extendsextends parent1{
+	public static void main(string[]args) {
+		
+	}
+}
